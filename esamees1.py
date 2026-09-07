5441c6768b020ae59d81757afc9058e2724cb5d8
AnnalisaMeneghini <meneghiniannalisa06@gmail.com>
2026-09-07 18:05:48 +0200

    Update esamees1.py

--- esamees1.py
+++ esamees1.py
@@ -32,3 +32,44 @@ def int_input():               #mettere la parentesi vuota perchè non richide d
 
 
 # 3. funzione che generi lista se n pari, va diviso per 2 se dispari, va moltiplicato per 3 e aggiunto 1. continuo fino a 1 o lista + 100 numeri
+def genera_sequenza(n):                      # n tra le arentesi a indicare che è necessario un parametro di ingresso 
+    sequenza = [n]                           # [n] a indicare una lista: ad esempio se n vale 4 la variabile "sequenza" diventa la lista [4]
+    
+    while n != 1 and len(sequenza) < 100:    # il simbolo != diverso da 1 continua il ciclo while 
+        if is_pari(n):
+            n = n // 2
+        else:                                # quando la condizione del if è falsa, cioè ho valori dispari 
+            n = 3 * n + 1
+            sequenza.append(n)               # appen(n) significa aggiungere in coda--allunga la lista preesistente, non creo una nuova lista
+        
+    return sequenza  
+
+# 4. funzione che analizza la sequenza e restituisce massimo,lunghezza e somma della sequenza
+def analizza_sequenza(lista):
+    massimo = lista[0]                       # creo la variabile "massimo", prendo elemento in posizione 0 (la prima) provvisorio 
+    somma = 0                                # sommo a 0 un numero (lo 0 non indice posizionale) 
+    for numero in lista:                     # prendo uno alla volta un elemento della lista e lo chiamo "numero" e poi eseguo somma
+        somma = somma + numero               
+        if numero > massimo:                 # "if" all'intrno del for cioè controllo ogni elemento. se fosse fuori lo eseguirei una volta dopo che ciclo è terminato 
+            massimo = numero                 # aggiorno il valore del massimo 
+    lunghezza = len(lista)
+    return massimo, lunghezza, somma
+
+
+# 5. funzione che ricerca nella lista i valori divisibili per 5 e li manda a schermo
+def ricerca(lista):
+    trovati = False
+
+    for numero in lista:                    #prendo in considerazione un numero alla volta 
+        if numero % 5 == 0:                 # resto % della divisione per 5 è pari a 0, condizione vera e stampo
+            print(numero)      
+            trovati = True
+
+    if trovati == False:                    # se non trovo nessun valore stampo 
+        print("Non ci sono numeri divisibili per 5 nella sequenza")
+
+
+# 6. 
+
+
+
